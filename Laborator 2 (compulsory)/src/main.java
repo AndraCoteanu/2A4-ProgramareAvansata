fd2d8d44dcf78efb223f2515c37a61e2360e7c85
Andra Coteanu <72747266+AndraCoteanu@users.noreply.github.com>
2021-02-27 20:57:55 +0200

    update

---
+++ Laborator 2 (compulsory)/src/main.java
@@ -0,0 +1,14 @@
+/* @autor: Andra C. */
+public class main {
+    public static void main(String[] args) {
+        /* declar un obiect de tipul Problem pentru ai putea accesa functionalitatile */
+        Problem problema = new Problem();
+
+        /* se initializeaza datele problemei */
+        problema.makeProblem();
+
+        /* se afiseaza datele problemei */
+        System.out.println(problema);
+        problema.printProblem();
+    }
+}
