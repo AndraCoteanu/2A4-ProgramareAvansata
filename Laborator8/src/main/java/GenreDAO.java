709d4ec2b89dc1020f80c380db8a93c7c659203d
Andra Coteanu <72747266+AndraCoteanu@users.noreply.github.com>
2021-04-16 18:45:54 +0300

    update

--- Laborator8/src/main/java/GenreDAO.java
+++
@@ -1,4 +0,0 @@
-public interface GenreDAO {
-    void create(int id_genre, String nume);
-    Genres findByName(String name);
-}
--- Laborator8/src/main/java/MovieDAO.java
+++
@@ -1,4 +0,0 @@
-public interface MovieDAO {
-    void create(int id_movie, String nume, int year, int duration, double score);
-    Movie findByName(String name);
-}
