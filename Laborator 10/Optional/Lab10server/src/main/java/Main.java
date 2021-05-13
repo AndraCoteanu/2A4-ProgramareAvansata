29718cd3cd1b1e89269219b1783be289a4c0795e
Andra Coteanu <72747266+AndraCoteanu@users.noreply.github.com>
2021-05-13 13:37:14 +0300

    upload optional

---
+++ Laborator 10/Optional/Lab10server/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+/**
+ * <h3> Clasa Main </h3>
+ * <p> Din clasa main se porneste serverul pentru reteaua sociala apeland un obiect de tipul SocialServer. </p>
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        SocialServer socialServer = new SocialServer();
+    }
+}
---
+++ Laborator 10/Optional/Lab10server/src/main/java/SocialServer.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <h3> Clasa SocialServer </h3>
+ * <p> Defineste portul de conectare al serverului. </p>
+ * <p> Serverul asteapta conexiuni din partea clientilor. </p>
+ * <p> Pentru fiecare client nou conectat se va crea un fir de executie / thread. </p>
+ */
+public class SocialServer {
+    public static final int PORT = 2121;
+    private static List<ClientThread> clients = new ArrayList();
+    public SocialServer() throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            serverSocket.setSoTimeout(180000);
+            while(true) {
+                System.out.println("Waiting :) ... ");
+                Socket socket = serverSocket.accept();
+                new ClientThread(socket).start();
+            }
+        } catch (IOException exception) {
+            System.err.println("Upsiez..." + exception);
+        } finally {
+            serverSocket.close();
+        }
+    }
+
+    public static List<ClientThread> getClients() {
+        return clients;
+    }
+}
